56881061c5913cf6e2047f9fa3f09969e500b75f
Fbolanos5 <fbolanos@csu.fullerton.edu>
2023-04-28 23:17:56 -0700

    Had to redownload nltk for this file should work

--- Main.py
+++ Main.py
@@ -1,16 +1,11 @@
+import pandas as pd
 import nltk
-from typing import List
-from pprint import pprint
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 
-stopwords = set(nltk.corpus.stopwords.words("english"))
-text = nltk.Text(nltk.corpus.state_union.words())
-concordance_list = text.concordance_list("america", lines = 2)
-#for entry in concordance_list:
- #   print(entry.line)
-words: List[str] = nltk.word_tokenize(
-     """Beautiful is better than ugly.
-     Explicit is better than implicit.
-    Simple is better than complex.""" )
-text = nltk.Text(words)
-fd = text.vocab()
-fd.tabulate(3)
+
+df = pd.read_csv('https://raw.githubusercontent.com/pycaret/pycaret/master/datasets/amazon.csv')
+
+print(df)
